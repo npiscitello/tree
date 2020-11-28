c8ec2c6e1cbfc4bc643395c8e71fa194c1602339
Nick <npiscitello@thelittlefish.net>
2020-11-28 16:24:50 -0500

    added ADC swapping to prep for speed control for patterns

--- main.c
+++ main.c
@@ -18,26 +18,33 @@
 volatile uint8_t flags = 0;
 #define F_PATTERN_CHANGED 7
 #define F_TIMER0_OVF      6
-#define F_ADC_DONE        5
-#define F_LED_STATE       4
+#define F_LED_STATE       5
 
-volatile uint8_t adc_val = 0;
+volatile uint8_t bright_val = 0;
+volatile uint8_t speed_val = 0;
+volatile uint8_t* adc_reg = &bright_val;
 
-// pattern switch ISR
-ISR(INT0_vect) {
-  flags |= _BV(F_PATTERN_CHANGED);
-}
-
-// timer 0 ISR
+// timer 0 ISR - swap LED banks
 ISR(TIMER0_OVF_vect) {
   flags |= _BV(F_TIMER0_OVF);
   flags ^= _BV(F_LED_STATE);
 }
 
-// ADC done ISR
+// timer 2 ISR - swap ADC regs and trigger an ADC conversion
+ISR(TIMER2_COMPA_vect) {
+  if( adc_reg == &bright_val ) {
+    adc_reg = &speed_val;
+    ADMUX = (ADMUX & 0xF0) | _BV(MUX0);
+  } else {
+    adc_reg = &bright_val;
+    ADMUX = (ADMUX & 0xF0);
+  }
+  ADCSRA |= _BV(ADSC);
+}
+
+// ADC done ISR - write out result
 ISR(ADC_vect) {
-  flags |= _BV(F_ADC_DONE);
-  adc_val = ADCH;
+  *adc_reg = ADCH;
 }
 
 int main(void) {
@@ -51,20 +58,18 @@ int main(void) {
   // turn on what we need, turn off what we don't
   //  - Timer 0: output PWM
   //  - Timer 1: blink/fade delay timing
+  //  - Timer 2: ADC management
   //  - ADC: read delay time/brightness potentiometer
-  PRR = ~_BV(PRTIM0) & ~_BV(PRTIM1) & ~_BV(PRADC);
+  PRR = ~_BV(PRTIM0) & ~_BV(PRTIM1) & ~_BV(PRTIM2) & ~_BV(PRADC);
 
-  // enable interrupts for timers, switches, ADC, etc.
-  EICRA = _BV(ISC00);
-  EIMSK = _BV(INT0);
+  // enable interrupts for timers and ADC
   asm("sei");
 
-  // set up ADC to take continuous readings on ADC0 against AREF
-  // and left adjust the result
-  ADMUX |= _BV(ADLAR);
-  ADCSRA |= _BV(ADATE) | _BV(ADIE) | _BV(ADPS2) | _BV(ADPS1);
+  // set up ADC in oneshot mode and left adjust the result
+  ADMUX = _BV(ADLAR);
+  // sys/64, 125kHz
+  ADCSRA = _BV(ADIE) | _BV(ADPS2) | _BV(ADPS1);
   ADCSRA |= _BV(ADEN);
-  ADCSRA |= _BV(ADSC);
 
   // set up PWM on timer 0
   // fast PWM, 256 bit resolution
@@ -74,6 +79,16 @@ int main(void) {
   // clear compare regs
   OCR0A = 0x00;
   OCR0B = 0x00;
+
+  // set up ADC switching on timer 2
+  // CTC
+  TCCR2A = _BV(WGM21);
+  // enable interrupts
+  TIMSK2 = _BV(OCIE2A);
+  // sys/64, OCR2A of 125 yields ~1000 triggers/sec
+  // At the ADC clock of 125kHz, that allows 125 ADC clocks per trigger
+  OCR2A = 125;
+  TCCR2B = _BV(CS22);
 
   // this only works b/c the steady/pattern switch is also
   // the power switch (SPDT w/ off position)
@@ -87,13 +102,8 @@ int main(void) {
     TCCR0B = _BV(CS00);
 
     while( 1 ) {
-      // read ADC to get brightness
-      if( flags & _BV(F_ADC_DONE) ) {
-        flags &= ~_BV(F_ADC_DONE);
-        OCR0A = adc_val;
-        OCR0B = adc_val;
-      }
-
+      OCR0A = bright_val;
+      OCR0B = bright_val;
       if( flags & _BV(F_TIMER0_OVF) ) {
         flags &= ~_BV(F_TIMER0_OVF);
         // LED outputs CANNOT be on simultaneously
@@ -114,12 +124,9 @@ int main(void) {
     TCCR0B = _BV(CS01);
 
     while( 1 ) {
-      // read ADC to get pattern speed
-      if( flags & _BV(F_ADC_DONE) ) {
-        flags &= ~_BV(F_ADC_DONE);
-      }
-
-      if( PIND &  PIN_PATTERN ) {
+      OCR0A = bright_val;
+      OCR0B = bright_val;
+      if( PIND & _BV(PIN_PATTERN) ) {
         // blink mode
 
       } else {
